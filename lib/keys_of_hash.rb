45c0850d869e820e53d11afaf41963ec9347812d
Lois Choi <insil321@hotmail.com>
2018-10-07 19:40:42 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,11 @@
 class Hash
   def keys_of(*arguments)
+    i=0
   	new_array = []
     self.each do |key, value|
     	if value == arguments
-    		 new_array << key
+    		 new_array << key[i]
+    		 i+=1
     	end
     end
     new_array
